58d7ce60ce567c31cd712bea7ee2e6b71af2b667
krashmi11 <kanharkarrashmi@gmail.com>
2023-12-01 23:22:38 +0530

    changes done

--- Creational/Prototype/Main.java
+++ Creational/Prototype/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Prototype p1;
-        p1 = new Student("Rashmi", 11, 9.83, "7020709427");
+        p1 = new Student("Rashmi", 11, 9.83, "6883784");
         p1.showRecord();
         Prototype s = p1.getclone();
         s.showRecord();
